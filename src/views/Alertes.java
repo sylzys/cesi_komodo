91ad3e5a56b9658cbf2bb12b448967b877043f25
BoBo <BoBo@10.176.249.115>
2012-10-15 23:27:57 +0200

    Linkage de la bdd avec la vue des alertes

--- src/views/Alertes.java
+++ src/views/Alertes.java
@@ -43,47 +43,34 @@ public class Alertes extends KContainer{
         listScroller.setPreferredSize(new Dimension(800, 600));
         listScroller.setBackground(new java.awt.Color(255, 255, 255));
         listScroller.getVerticalScrollBar().setUnitIncrement(10);
-        JPanel buttonPane = new JPanel();
-        buttonPane.setLayout(new BoxLayout(buttonPane, BoxLayout.Y_AXIS));
-        buttonPane.setBorder(javax.swing.BorderFactory.createEmptyBorder(20, 20, 20, 20));
-        buttonPane.setBackground(new java.awt.Color(255, 255, 255));
+        JPanel PanelListPane = new JPanel();
+        PanelListPane.setLayout(new BoxLayout(PanelListPane, BoxLayout.Y_AXIS));
+        PanelListPane.setBorder(javax.swing.BorderFactory.createEmptyBorder(20, 20, 20, 20));
+        PanelListPane.setBackground(new java.awt.Color(255, 255, 255));
         
-        JPanel jp = new JPanel();
-        jp.setPreferredSize(new Dimension(730, 70));
-        jp.setMinimumSize(new Dimension(730, 70));
-        jp.setBorder(javax.swing.BorderFactory.createEtchedBorder());
-        //jp.setLayout(new BoxLayout(jp, BoxLayout.X_AXIS));
-        Box kikoo = Box.createHorizontalBox();
-        kikoo.add(new JLabel("foo"));
-        kikoo.add(Box.createHorizontalStrut(25));
-        kikoo.add(new JLabel("bar"));
-        kikoo.add(Box.createHorizontalStrut(25));
-        kikoo.add(new JButton("slop"));
-        jp.add(kikoo);
-        
+        // get alertes
         AlerteInstance AlertInstance = AlerteInstance.getInstance();
-        
-        Hashtable h = new Hashtable();
-        //h.put("comid", 1);
-        l_alerts = AlertInstance.GetAlertes("", h);
+        l_alerts = AlertInstance.GetAlertes("", new Hashtable());
         System.out.println("nb_alerts : " + l_alerts.size());
         for (Suivdossier tmp : l_alerts) {
-            System.out.println(tmp.getSuivdoscom());
+            JPanel jp = new JPanel();
+            jp.setPreferredSize(new Dimension(730, 70));
+            jp.setMinimumSize(new Dimension(730, 70));
+            jp.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+            //jp.setLayout(new BoxLayout(jp, BoxLayout.X_AXIS));
+            Box kikoo = Box.createHorizontalBox();
+            kikoo.add(new JLabel(tmp.getSuivdosdate().toString()));
+            kikoo.add(Box.createHorizontalStrut(25));
+            kikoo.add(new JLabel(tmp.getSuivdoscom()));
+            kikoo.add(Box.createHorizontalStrut(25));
+            kikoo.add(new JButton("details"));
+            jp.add(kikoo);        
+            PanelListPane.add(jp);
+            PanelListPane.add(Box.createRigidArea(new Dimension(0, 20)));
         }
         
-        
-        JPanel jp2 = new JPanel();
-        jp2.setPreferredSize(new Dimension(730, 70));
-        jp2.setBorder(javax.swing.BorderFactory.createEtchedBorder());
-        buttonPane.add(jp);
-        buttonPane.add(Box.createRigidArea(new Dimension(0, 20)));
-        buttonPane.add(jp2);
-        buttonPane.add(Box.createRigidArea(new Dimension(10, 600)));
-        buttonPane.add(new JLabel("kikoo lol"));
-        
-        listScroller.setViewportView(buttonPane);
+        listScroller.setViewportView(PanelListPane);
         //listPane.add(listScroller, BorderLayout.PAGE_START);
-        
         this.panel.add(listScroller);
     }
 }
