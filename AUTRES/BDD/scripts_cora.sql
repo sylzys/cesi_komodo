a4ade5044b5011d1c71fb13b18bced77d1021836
suly <suly@suly-PC>
2012-10-30 22:12:12 +0100

    Avancement synchro

--- AUTRES/BDD/scripts_cora.sql
+++ AUTRES/BDD/scripts_cora.sql
@@ -1,16 +1,16 @@
-﻿UPDATE "public"."client" SET "clitel"='0232101658', "climail"='jfdnhojd@djfhnkdj.fr', "cliactivite"='Mecanique auto', "clisiret"='98787687' WHERE ("cliid"='1')
+﻿UPDATE "public"."client" SET "clitel"='0232101658', "climail"='jfdnhojd@djfhnkdj.fr', "cliactivite"='Mecanique auto', "clisiret"='98787687' WHERE ("cliid"='1');
 
 
-INSERT INTO "public"."demande" ("demandeid", "cliid", "utiid", "demandeetat", "demandedteadd") VALUES ('2', '1', '1', '60', '2012-10-27')
+INSERT INTO "public"."demande" ("cliid", "utiid", "demandeetat", "demandedteadd") VALUES ('1', '1', '60', '2012-10-27');
 
-INSERT INTO "public"."suivdossier" ("interid", "demandeid", "devid", "utiid", "suivdosdate", "suivdoscom", "suividossuppr") VALUES ('1', '2', '2', '1', '2012-10-19', 'Blbalblabalblaa', 'f')
+INSERT INTO "public"."suivdossier" ("interid", "demandeid", "devid", "utiid", "suivdosdate", "suivdoscom", "suividossuppr") VALUES ('1', '2', '2', '1', '2012-10-19', 'Blbalblabalblaa', 'f');
 
-INSERT INTO "public"."suivdossier" ("interid", "demandeid", "devid", "utiid", "suivdosdate", "suivdoscom", "suividossuppr") VALUES ('1', '2', '3', '1', '2012-10-27', 'Kihfzcozehd ezufhozu uoczhfcokze', 'f')
+INSERT INTO "public"."suivdossier" ("interid", "demandeid", "devid", "utiid", "suivdosdate", "suivdoscom", "suividossuppr") VALUES ('1', '2', '3', '1', '2012-10-27', 'Kihfzcozehd ezufhozu uoczhfcokze', 'f');
 
-UPDATE "public"."devis" SET "devetat"='refusée' WHERE ("devid"='2')
-UPDATE "public"."devis" SET "devetat"='en attente' WHERE ("devid"='3')
+UPDATE "public"."devis" SET "devetat"='refusée' WHERE ("devid"='2');
+UPDATE "public"."devis" SET "devetat"='en attente' WHERE ("devid"='3');
 
-CREATE OR REPLACE VIEW "public"."getDetailsDemande" AS 
+CREATE OR REPLACE VIEW "public"."detailsdemande" AS 
 SELECT
 "public".demande.demandeid,
 "public".client.cliid,
