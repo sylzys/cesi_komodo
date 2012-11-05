2883413cebc1448c713f384f887ff20db5cc9a40
lavie <lavie@STATION287.GASNY.loc>
2012-11-05 11:29:59 +0100

    Avancement Synchro

--- src/controllers/Synchro.java
+++ src/controllers/Synchro.java
@@ -250,6 +250,7 @@ public class Synchro {
     //Envoi des requete dans la base en ligne
     public void record()
     {
+        boolean test = false;
         boolean empty = emptyFic();
         if(empty == true)
         {
@@ -276,16 +277,23 @@ public class Synchro {
                     Query q = HibernateConnection.getSession().createSQLQuery(requete);
                     q.executeUpdate();
                     tx.commit();
-                    eraseFic();
-                    JOptionPane jop = new JOptionPane();
-                    jop.showMessageDialog(null, "Vous êtes synchronisé avec la base en ligne", "Confirmation", JOptionPane.INFORMATION_MESSAGE);
+                    eraseFic();   
+                    test = true;
                     Fenetre fen = Fenetre.getInstance();
                     fen.RenewAccueil();
                 } catch (HibernateException ex) {
-                    System.out.println(ex.toString());
-                    JOptionPane jop = new JOptionPane();
-                    jop.showMessageDialog(null, "Une erreur est survenue lors de la synchronisation", "Erreur", JOptionPane.INFORMATION_MESSAGE);
+                    test = false;
+                    System.out.println(ex.toString());                    
                 }  
+            }
+            JOptionPane jop = new JOptionPane();
+            if(test == true)
+            {
+                jop.showMessageDialog(null, "Vous êtes synchronisé avec la base en ligne", "Confirmation", JOptionPane.INFORMATION_MESSAGE);
+            }
+            else
+            {
+                jop.showMessageDialog(null, "Une erreur est survenue lors de la synchronisation", "Erreur", JOptionPane.INFORMATION_MESSAGE);
             }
         }
     }
