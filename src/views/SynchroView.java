fa0f89d8cf06677bd92ae5426861936623977c4e
lavie <lavie@STATION287.GASNY.loc>
2012-11-06 09:48:22 +0100

    Avancement Synchro

--- src/views/SynchroView.java
+++ src/views/SynchroView.java
@@ -18,7 +18,9 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.ArrayList;
 import java.util.Hashtable;
+import java.util.List;
 import java.util.StringTokenizer;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -27,14 +29,18 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.border.EmptyBorder;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 /**
  *
- * @author sylv
+ * @author slavie
  */
 public class SynchroView extends KContainer {
     private String req;
     private String fic;
     private String libreq;
+    private JCheckBox chkslct = new JCheckBox();
+    private List<CheckBoxData> listchk;
     JLabel title = new JLabel ();
     public SynchroView(UserActif user) {
     super();
@@ -45,6 +51,7 @@ public class SynchroView extends KContainer {
     @Override
     protected
     void initPanel() {
+        listchk = new ArrayList<CheckBoxData>();
         JPanel content = new JPanel();
         JPanel center = new JPanel();
         JScrollPane scroll = new JScrollPane(center);
@@ -53,11 +60,12 @@ public class SynchroView extends KContainer {
         JButton button = new JButton("Synchroniser"); 
         JLabel lblSynchro = new JLabel(new ImageIcon("ressources/images/synchro.png"));
         Synchro sync = new Synchro();        
-        titre.setBackground(Color.white);
+        titre.setOpaque(false);
+        pnlbtn.setOpaque(false);
         titre.add(title, BorderLayout.CENTER);
-        content.setBackground(Color.white);
+        center.setOpaque(false);
+        content.setOpaque(false);
         content.setLayout(new BorderLayout());
-        center.setBackground(Color.white);
         center.setLayout(new BorderLayout());
         scroll.setPreferredSize(new Dimension(800,500));
         center.scrollRectToVisible(new Rectangle(0,center.getHeight(),10,10));
@@ -71,27 +79,29 @@ public class SynchroView extends KContainer {
         else if(sync.emptyFic() == true)
         {      
             FlowLayout flchk1 = new FlowLayout();
-            JLabel lblempty = new JLabel();
             JLabel lbl1 = new JLabel("<html><p color=blue>CLIENT</p></html>");
             JLabel lbl2 = new JLabel("<html><p color=blue>TABLE</p></html>");
             JLabel lbl3 = new JLabel("<html><p color=blue>ACTION</p></html>");
             lbl1.setBorder(new EmptyBorder(0, 10, 0, 0));
             lbl2.setBorder(new EmptyBorder(0, 10, 0, 0));
             lbl3.setBorder(new EmptyBorder(0, 10, 0, 0));
-            lblempty.setBorder(new EmptyBorder(0, 10, 0, 0));
-            lblempty.setBackground(Color.lightGray);
+            chkslct.setBorder(new EmptyBorder(0, 10, 0, 0));
+            chkslct.setOpaque(false);
             lbl1.setOpaque(true);
             lbl1.setBackground(Color.lightGray);
             lbl2.setOpaque(true);
             lbl2.setBackground(Color.lightGray);
             lbl3.setOpaque(true);
             lbl3.setBackground(Color.lightGray);
-            lblempty.setPreferredSize(new Dimension(100, 20));
+            chkslct.setOpaque(true);
+            chkslct.setBackground(Color.lightGray);
+            chkslct.setPreferredSize(new Dimension(70, 20));
+            chkslct.setSelected(true);
             lbl1.setPreferredSize(new Dimension(180, 20));
-            lbl2.setPreferredSize(new Dimension(150, 20));
+            lbl2.setPreferredSize(new Dimension(180, 20));
             lbl3.setPreferredSize(new Dimension(300, 20));
             center.setLayout(flchk1);
-            center.add(lblempty);
+            center.add(chkslct);
             center.add(lbl1);
             center.add(lbl2);
             center.add(lbl3);
@@ -115,22 +125,19 @@ public class SynchroView extends KContainer {
                 }
                 String[] action = sync.readReq(req, interid, nomclient);
                 FlowLayout flchk = new FlowLayout();
-                JCheckBox chkbox = new JCheckBox();
-                // utilisation du checkbox avec id
-                CheckBoxData chk = new CheckBoxData(42, "requete");
-                System.out.println(chk.getId());
-                // fin demo utilisation
-                JLabel lblclient = new JLabel(action[0]);
+                CheckBoxData chkbox = new CheckBoxData(i, req);
+                chkbox.setSelected(true);
+                listchk.add(chkbox);
+                JLabel lblclient = new JLabel(action[0].toUpperCase());
                 JLabel lbltable = new JLabel(action[1].toUpperCase());
-                JLabel lblaction = new JLabel(action[2]);
-                chkbox.setBackground(Color.white);
+                JLabel lblaction = new JLabel(action[2].toUpperCase());
                 lblclient.setBorder(new EmptyBorder(0, 10, 0, 0));
                 lbltable.setBorder(new EmptyBorder(0, 10, 0, 0));
                 lblaction.setBorder(new EmptyBorder(0, 10, 0, 0));
                 chkbox.setBorder(new EmptyBorder(0, 10, 0, 0));
-                chkbox.setPreferredSize(new Dimension(100, 35));
+                chkbox.setPreferredSize(new Dimension(70, 35));
                 lblclient.setPreferredSize(new Dimension(180, 35));
-                lbltable.setPreferredSize(new Dimension(150, 35));
+                lbltable.setPreferredSize(new Dimension(180, 35));
                 lblaction.setPreferredSize(new Dimension(300, 35));
                 center.setLayout(flchk);
                 center.add(chkbox);
@@ -140,10 +147,30 @@ public class SynchroView extends KContainer {
                 chkbox.addActionListener(new ActionListener() {
                     @Override
                     public void actionPerformed(ActionEvent e) {
-                        System.out.println(req);
+                         CheckBoxData chk = (CheckBoxData)e.getSource();
+                        System.out.println(chk.getId());
                     }
                 });
             }
+            chkslct.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    if(chkslct.isSelected() == true)
+                    {
+                         for(CheckBoxData chkdata: listchk)
+                         {
+                             chkdata.setSelected(true);
+                         }
+                    }
+                    else
+                    {
+                        for(CheckBoxData chkdata: listchk)
+                         {
+                             chkdata.setSelected(false);
+                         }
+                    }
+                }
+            });
             center.setPreferredSize(new Dimension(750, 50 + (40 * i)));
             title.setPreferredSize(new Dimension(420,100)); 
             title.setText("<html><center><h2>Synchronisation de la base de données</h2><br><p color=green>Sélectionnez "
