d2b12169e8fc92031c1c278af17e244817b1852d
Syl Zys <sylzys@gmail.com>
2012-11-07 09:09:26 +0100

    branche livrable 1

--- AUTRES/BDD/2 - fil_rouge__create_database.sql
+++
@@ -1,1 +0,0 @@
-CREATE DATABASE projetcesi WITH TEMPLATE template0;
--- ressources/replicationBDD/save.bat
+++
@@ -1,5 +0,0 @@
-pg_dump -h 127.0.0.1 -p 5432 -U cesi -Fc -f "save.bak" "projetcesi"
-dropdb -h localhost -p 5432 -U cesi "projetcesi2"
-createdb -T postgres -h localhost -p 5432 -U cesi "projetcesi2"
-pg_restore -h localhost -p 5432 -U cesi  -d "projetcesi2" -v "save.bak"
-pause
